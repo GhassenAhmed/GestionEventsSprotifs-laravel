8680976e002e4a4e2e0711fb36105befbaf4d426
ghassen <ghassenahmed74@gmail.com>
2023-03-16 09:43:26 +0100

    create column table evenement and categorie

--- database/migrations/2023_03_16_082256_create_categories_table.php
+++ database/migrations/2023_03_16_082256_create_categories_table.php
@@ -15,6 +15,9 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('nom',100);
+            $table->enum('sexe',['HOME','FEMME']);
+            $table->string('poids',20);
             $table->timestamps();
         });
     }
