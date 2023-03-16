86948d13cdbac118767652aa1a6065636fe703a9
ghassen <ghassenahmed74@gmail.com>
2023-03-16 10:26:49 +0100

    relation belongsTo evennement

--- app/Models/EvenementSportif.php
+++ app/Models/EvenementSportif.php
@@ -1,11 +1,14 @@
 <?php
 
 namespace App\Models;
-
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class EvenementSportif extends Model
 {
     use HasFactory;
+    public function organisateur(){
+        return $this->belongsTo(User::class);
+    }
 }
