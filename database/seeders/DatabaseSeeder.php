556d9fcd5a28b05c6c2c790f77a1cde6596bb929
ghassen <101054191+GhassenAhmed@users.noreply.github.com>
2023-04-06 00:30:44 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,37 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-       
+        User::factory(2)
+        ->has(EvenementSportif::factory()
+                ->count(1)
+                ->has(Categorie::factory()
+                ->count(3)
+                ->state(new Sequence(
+                    ['nom'=>'Minim'],
+                    ['nom'=>'Cadet'],
+                    ['nom'=>'Senior'],
+                ))
+                ->state(new Sequence(
+                    ['sexe'=>'HOMME'],
+                    ['sexe'=>'FEMME'],
+                ))
+                ->state(new Sequence(
+                    ['poids'=>'-40 KG'],
+                    ['poids'=>'-50 KG'],
+                    ['poids'=>'+50 KG'],
+                ))
+                ->has(Athlete::factory()
+                    ->count(2)
+                    ->state(function(array $attributes,Categorie $categorie){
+                        return ['sexe'=>$categorie->sexe];
+                    })
+                    ->for(Equipe::factory())
+                    ->hasCommentaires(2)
+                )
+                )
+                ->hasCommentaires(2)
+        )
+        ->create();
 
     }
 }
